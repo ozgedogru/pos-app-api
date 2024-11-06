5787c7542220fd0bb7abe6ebed8909a9f137517f
ozgedogru <ozgenurkarademir@gmail.com>
2024-11-06 17:27:56 +0100

    correction

--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const ProductSchema = mongoose.Schema(
   {
     title: { type: String, require: true },
-    image: { type: String, require: true },
+    img: { type: String, require: true },
     price: { type: Number, require: true },
     category: { type: String, require: true },
   },
